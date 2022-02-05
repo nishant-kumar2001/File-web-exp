0a12668eb32993da1e672cf5d8d1ab1168b2ae07
adityagpramanik <adityagpramanik@gmail.com>
2022-02-05 18:24:26 +0530

    front-end: elements from stream and layout

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # File-web-exp
+
+#### Initial Config of client & server
+Make sure your WebDAV server is running and GET, PUT, DELETE ... methods are open to all.
+
+1. Run commands below for initial server config
+```
+cd server/
+npm i
+npm test
+```
+2. Run commands below for initial server config
+```
+cd file-web-exp/
+npm i
+#click on live server for frontend
+```
---
+++ server/index.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const { AuthType, createClient } = require("webdav");
+const express = require("express");
+const app = express();
+const cors = require('cors');
+// --------------------Modules----------------------
+
+// WebDAV connection
+const client = createClient("http://localhost:80/webdav/", {
+        authType: AuthType.Digest,
+        username: "fakeuser",
+        password: "strongKey@221"
+    });
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send("Hello world!");
+});
+
+app.post('/mkdir', async (req, res) => {
+    const {path} = req.body;
+
+    await client.createDirectory(path);
+    res.send(`directory ${path}`);
+});
+
+app.listen(3001, () => {
+    console.log("Server started listening at port 3001");
+})
+
+// const directoryItems = async () => await client.getDirectoryContents("/");
+
+// const uploadLink = client.getFileUploadLink("/image.png");
+// console.log(directoryItems());
+// mkDir();
